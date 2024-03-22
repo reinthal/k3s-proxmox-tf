7624765bdc753205db9f68782d8ed3afe041d37a
alexander reinthal <alexander.reinthal@github.noreply.lolz>
2024-03-22 23:55:13 +0100

    configure k3s hosts

--- main.tf
+++ main.tf
@@ -1,7 +1,79 @@
 # Create Proxmox VMs
 
-resource "proxmox_vm_qemu" "kubernetes_nodes" {
+resource "proxmox_vm_qemu" "pvc_kubernetes_nodes" {
+  for_each = var.pvc_node_configs
+
+  name        = each.key
+  vmid        = each.value["vm_id"]
+  target_node = "pvc"                # Change to the desired Proxmox node
+  clone       = "ubuntu-22-04-jammy" # Change to the name of the template or VM to clone from
+  cores       = each.value["cores"]
+  memory      = each.value["ram"]
+  scsihw      = "virtio-scsi-single"
+  tags        = "k3s"
+  os_type   = "cloud-init"
+
+  network {
+    bridge    = "vmbr0"
+    firewall  = false
+    link_down = false
+    model     = "virtio"
+    tag       = 20
+
+  }
+  disks {
+    scsi {
+      scsi0 {
+        disk {
+          cache    = "writeback"
+          size     = each.value["hard_drive"]
+          backup   = true
+          storage  = "local-zfs"
+          iothread = true
+        }
+
+      }
+    }
+  }
+}
+resource "proxmox_vm_qemu" "pvd_kubernetes_nodes" {
   for_each = var.pvd_node_configs
+
+  name        = each.key
+  vmid        = each.value["vm_id"]
+  target_node = "pvd"                # Change to the desired Proxmox node
+  clone       = "ubuntu-22-04-jammy" # Change to the name of the template or VM to clone from
+  cores       = each.value["cores"]
+  memory      = each.value["ram"]
+  scsihw      = "virtio-scsi-single"
+  tags        = "k3s"
+  os_type   = "cloud-init"
+
+  network {
+    bridge    = "vmbr0"
+    firewall  = false
+    link_down = false
+    model     = "virtio"
+    tag       = 20
+
+  }
+  disks {
+    scsi {
+      scsi0 {
+        disk {
+          cache    = "writeback"
+          size     = each.value["hard_drive"]
+          backup   = true
+          storage  = "local-zfs"
+          iothread = true
+        }
+
+      }
+    }
+  }
+}
+resource "proxmox_vm_qemu" "pve_kubernetes_nodes" {
+  for_each = var.pve_node_configs
 
   name        = each.key
   vmid        = each.value["vm_id"]
@@ -11,7 +83,8 @@ resource "proxmox_vm_qemu" "kubernetes_nodes" {
   memory      = each.value["ram"]
   scsihw      = "virtio-scsi-single"
   tags        = "k3s"
-  
+  os_type   = "cloud-init"
+
   network {
     bridge    = "vmbr0"
     firewall  = false
--- variables.tf
+++ variables.tf
@@ -7,17 +7,16 @@ variable "pve-password" {
 variable "pvc_node_configs" {
   default = {
     "dutch-3" = { vm_id = 2013, cores = 4, ram = 4096, hard_drive = 50 }
-    "revy-3"  = { vm_id = 2023, cores = 8, ram = 8096, hard_drive = 50 }
-    "rock-2"  = { vm_id = 2032, cores = 8, ram = 4096, hard_drive = 250 }
-    "boris-1" = { vm_id = 2041, cores = 8, ram = 4096, hard_drive = 250 }
+    "rock-2"  = { vm_id = 2032, cores = 12, ram = 8096, hard_drive = 100 }
+    "boris-3" = { vm_id = 2043, cores = 12, ram = 4096, hard_drive = 300 }
   }
 }
 variable "pvd_node_configs" {
   default = {
     "dutch-2" = { vm_id = 2012, cores = 4, ram = 4096, hard_drive = 50 }
     "revy-2"  = { vm_id = 2022, cores = 4, ram = 8096, hard_drive = 50 }
-    "rock-1"  = { vm_id = 2031, cores = 4, ram = 4096, hard_drive = 200 }
-    "boris-2" = { vm_id = 2042, cores = 4, ram = 4096, hard_drive = 200 }
+    "rock-1"  = { vm_id = 2031, cores = 4, ram = 4096, hard_drive = 100 }
+    "boris-2" = { vm_id = 2042, cores = 4, ram = 4096, hard_drive = 300 }
   }
 }
 
@@ -25,6 +24,7 @@ variable "pve_node_configs" {
   default = {
     "dutch-1" = { vm_id = 2011, cores = 4, ram = 4096, hard_drive = 50 }
     "revy-1"  = { vm_id = 2021, cores = 4, ram = 4096, hard_drive = 50 }
+    "boris-1" = { vm_id = 2041, cores = 4, ram = 4096, hard_drive = 300 }
   }
 
 }
