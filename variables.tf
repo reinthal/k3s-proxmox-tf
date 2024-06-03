aaeab4de4efb18a11e3aac1f4abd4fe85b53e728
alexander reinthal <alexander.reinthal@github.noreply.lolz>
2024-06-03 15:01:41 +0200

    downsize cluster

--- variables.tf
+++ variables.tf
@@ -13,25 +13,21 @@ variable "proxmox_api_token_secret" {
 # Define variables for the VM configurations
 variable "pvc_node_configs" {
   default = {
-    "dutch-3" = { vm_id = 2013, cores = 4, ram = 4096, hard_drive = 50 }
-    "rock-2"  = { vm_id = 2032, cores = 12, ram = 8096, hard_drive = 100 }
-    "boris-3" = { vm_id = 2043, cores = 12, ram = 4096, hard_drive = 500 }
+    "dutch-c" = { vm_id = 2013, cores = 4, ram = 4096, hard_drive = 50 }
+    "revy-c"  = { vm_id = 2023, cores = 12, ram = 8096, hard_drive = 100 }
   }
 }
 variable "pvd_node_configs" {
   default = {
-    "dutch-2" = { vm_id = 2012, cores = 4, ram = 4096, hard_drive = 50 }
-    "revy-2"  = { vm_id = 2022, cores = 4, ram = 8096, hard_drive = 50 }
-    "rock-1"  = { vm_id = 2031, cores = 4, ram = 4096, hard_drive = 100 }
-    "boris-2" = { vm_id = 2042, cores = 4, ram = 4096, hard_drive = 500 }
+    "dutch-d" = { vm_id = 2012, cores = 4, ram = 4096, hard_drive = 50 }
+    "revy-d"  = { vm_id = 2022, cores = 4, ram = 8096, hard_drive = 100 }
   }
 }
 
 variable "pve_node_configs" {
   default = {
-    "dutch-1" = { vm_id = 2011, cores = 4, ram = 4096, hard_drive = 50 }
-    "revy-1"  = { vm_id = 2021, cores = 4, ram = 4096, hard_drive = 50 }
-    "boris-1" = { vm_id = 2041, cores = 4, ram = 4096, hard_drive = 500 }
+    "dutch-e" = { vm_id = 2011, cores = 4, ram = 4096, hard_drive = 50 }
+    "revy-e"  = { vm_id = 2021, cores = 4, ram = 4096, hard_drive = 100 }
   }
 
 }
