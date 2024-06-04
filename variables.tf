535f3f397905993a54f50fe0cffad0acffd01983
alexander reinthal <alexander.reinthal@github.noreply.lolz>
2024-06-04 11:09:40 +0200

    move c to pvd and pve

--- variables.tf
+++ variables.tf
@@ -13,14 +13,15 @@ variable "proxmox_api_token_secret" {
 # Define variables for the VM configurations
 variable "pvc_node_configs" {
   default = {
-    "dutch-c" = { vm_id = 2013, cores = 4, ram = 4096, hard_drive = 50, macaddr = "42:69:CE:F4:12:AE" }
-    "revy-c"  = { vm_id = 2023, cores = 12, ram = 8096, hard_drive = 100, macaddr = "1a:9b:20:dc:d6:c8"}
+   # "dutch-c" = { vm_id = 2013, cores = 4, ram = 4096, hard_drive = 50, macaddr = "42:69:CE:F4:12:AE" }
+  #  "revy-c"  = { vm_id = 2023, cores = 12, ram = 8096, hard_drive = 100, macaddr = "1a:9b:20:dc:d6:c8"}
   }
 }
 variable "pvd_node_configs" {
   default = {
+    "dutch-c" = { vm_id = 2013, cores = 4, ram = 4096, hard_drive = 50, macaddr = "42:69:CE:F4:12:AE" }
     "dutch-d" = { vm_id = 2012, cores = 4, ram = 4096, hard_drive = 50, macaddr = "be:0f:4d:3e:c6:03" }
-    "revy-d"  = { vm_id = 2022, cores = 4, ram = 8096, hard_drive = 100 , macaddr = "26:e4:6c:4e:01:1b"}
+    "revy-d"  = { vm_id = 2022, cores = 4, ram = 4096, hard_drive = 100 , macaddr = "26:e4:6c:4e:01:1b"}
   }
 }
 
@@ -28,6 +29,7 @@ variable "pve_node_configs" {
   default = {
     "dutch-e" = { vm_id = 2011, cores = 4, ram = 4096, hard_drive = 50 , macaddr = "82:b0:13:fe:4d:32"}
     "revy-e"  = { vm_id = 2021, cores = 4, ram = 4096, hard_drive = 100 , macaddr = "0a:03:1a:99:a7:c4"}
+    "revy-c"  = { vm_id = 2023, cores = 4, ram = 8096, hard_drive = 100, macaddr = "1a:9b:20:dc:d6:c8"}
   }
 
 }
