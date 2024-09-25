b4bd11b6f0fc702706db6b3cdc81ff1b5acca4e9
Alexander Reinthal <alexander.reinthal@portgot.se>
2024-09-25 15:56:01 +0200

    fix: parametrize cloud-initzzz

--- proxmox/main.tf
+++ proxmox/main.tf
@@ -5,9 +5,10 @@ data "local_file" "ssh_public_key" {
 }
 
 resource "proxmox_virtual_environment_file" "cloud_config" {
+  for_each = toset(["pve", "pvd", "pvc"])
   content_type = "snippets"
   datastore_id = "local"
-  node_name    = "pve"
+  node_name    = each.value
 
   source_raw {
     data = <<-EOF
@@ -60,7 +61,7 @@ resource "proxmox_virtual_environment_vm" "k3s" {
 
   initialization {
     datastore_id = each.value["datastore"]
-    user_data_file_id = proxmox_virtual_environment_file.cloud_config.id
+    user_data_file_id = proxmox_virtual_environment_file.cloud_config[each.value["node"]].id
     ip_config {
       ipv4 {
         address = "dhcp"
@@ -90,125 +91,3 @@ resource "proxmox_virtual_environment_vm" "k3s" {
     bridge = each.value["bridge"]
   }
 }
-
-/**
-resource "proxmox_vm_qemu" "pvc_kubernetes_nodes" {
-  for_each = var.pvc_node_configs
-
-  name                    = each.key
-  vmid                    = each.value["vm_id"]
-  target_node             = "pvc"                # Change to the desired Proxmox node
-  clone                   = "ubuntu-22-04-jammy" # Change to the name of the template or VM to clone from
-  cores                   = each.value["cores"]
-  memory                  = each.value["ram"]
-  scsihw                  = "virtio-scsi-single"
-  tags                    = "k3s"
-  os_type                 = "cloud-init"
-  ipconfig0               = "ip=dhcp,ip6=dhcp"
-  cloudinit_cdrom_storage = "local-zfs"
-  ssh_user                = "kog"
-  sshkeys                 = var.ssh_keys
-
-  network {
-    bridge    = "k3s"
-    firewall  = false
-    link_down = false
-    model     = "virtio"
-    macaddr = each.value["macaddr"]
-
-  }
-  disks {
-    scsi {
-      scsi0 {
-        disk {
-          cache    = "writeback"
-          size     = each.value["hard_drive"]
-          backup   = true
-          storage  = "local-zfs"
-          iothread = true
-        }
-
-      }
-    }
-  }
-}
-resource "proxmox_vm_qemu" "pvd_kubernetes_nodes" {
-  for_each = var.pvd_node_configs
-
-  name                    = each.key
-  vmid                    = each.value["vm_id"]
-  target_node             = "pvd"                # Change to the desired Proxmox node
-  clone                   = "ubuntu-22-04-jammy" # Change to the name of the template or VM to clone from
-  cores                   = each.value["cores"]
-  memory                  = each.value["ram"]
-  scsihw                  = "virtio-scsi-single"
-  tags                    = "k3s"
-  os_type                 = "cloud-init"
-  ipconfig0               = "ip=dhcp,ip6=dhcp"
-  cloudinit_cdrom_storage = "local-zfs"
-  ssh_user                = "kog"
-  sshkeys                 = var.ssh_keys
-  network {
-    bridge    = "k3s"
-    firewall  = false
-    link_down = false
-    model     = "virtio"
-    macaddr = each.value["macaddr"]
-  }
-  disks {
-    scsi {
-      scsi0 {
-        disk {
-          cache    = "writeback"
-          size     = each.value["hard_drive"]
-          backup   = true
-          storage  = "local-zfs"
-          iothread = true
-        }
-
-      }
-    }
-  }
-}
-resource "proxmox_vm_qemu" "pve_kubernetes_nodes" {
-  for_each = var.pve_node_configs
-
-  name                    = each.key
-  vmid                    = each.value["vm_id"]
-  target_node             = "pve"                # Change to the desired Proxmox node
-  clone                   = "ubuntu-22-04-jammy" # Change to the name of the template or VM to clone from
-  cores                   = each.value["cores"]
-  memory                  = each.value["ram"]
-  scsihw                  = "virtio-scsi-single"
-  tags                    = "k3s"
-  os_type                 = "cloud-init"
-  ipconfig0               = "ip=dhcp,ip6=dhcp"
-  cloudinit_cdrom_storage = "old-lvm"
-  ssh_user                = "kog"
-  sshkeys                 = var.ssh_keys
-
-  network {
-    bridge    = "k3s"
-    firewall  = false
-    link_down = false
-    model     = "virtio"
-    macaddr = each.value["macaddr"]
-  }
-  disks {
-
-    scsi {
-      scsi0 {
-        disk {
-          cache    = "writeback"
-          size     = each.value["hard_drive"]
-          backup   = true
-          storage  = "old-lvm"
-          iothread = true
-        }
-
-      }
-    }
-  }
-}
-
-*/
